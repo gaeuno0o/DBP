214c84d147cddf192a49e5e17ba4f561c6bbf7e2
gaeuno0o <20172020@sungshin.ac.kr>
2020-12-14 13:34:39 +0000

    add w06-P

---
+++ w06-P/emp_insert.php
@@ -0,0 +1,45 @@
+<?php
+    $link = mysqli_connect("localhost", "admin","admin","employees");    
+    $query = "SELECT MAX(emp_no) FROM employees";
+    $result = mysqli_query($link,$query);
+    $row = mysqli_fetch_array($result);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>직원 관리 시스템</title>
+</head>
+
+<body>
+    <h2><a href="index.php">직원 관리 시스템 </a> | 신규 직원 등록</h2><br>
+    <form action="emp_insert_process.php" method="post">
+        <label>emp_no:</label>
+        <input type="text" name="emp_no" placeholder="emp_no">
+        (last_num : <?=$row[0]?> ) 
+        <br>
+
+        <label>birth_date:(0000-00-00):</label>
+        <input type="text" name="birth_date" placeholder="birth_date"><br>
+
+        <label>first_name:</label>
+        <input type="text" name="first_name" placeholder="first_name"><br>
+
+        <label>last_name:</label>
+        <input type="text" name="last_name" placeholder="last_name"><br>
+        
+        <label>gender:(M or F)</label>
+        <select name = "gender">
+            <option value ="">gender</option>
+            <option value = "F">Female</option>
+            <option value = "M">Male</option>
+        </select><br>
+
+        <label>hire_date:(0000-00-00):</label>
+        <input type="text" name="hire_date" placeholder="hire_date"><br>
+        <input type="submit" value="Create">
+    </form>
+
+</body>
+</html>
